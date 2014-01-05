5982ce8b2336d273f76b8c65f5043b89e3f0c2ab
Arnelle Balane <arnellebalane@gmail.com>
2014-01-05 09:41:32 +0800

    dont show ignored apps in listing

--- javascripts/application.js
+++ javascripts/application.js
@@ -6,18 +6,25 @@ app.controller('AppsController', function($scope, $http) {
   $scope.ignore = function(event, index) {
     event.preventDefault();
     $scope.ignored.push($scope.apps[index]);
-    chrome.storage.local.set({'ignored': $scope.ignored});
+    $scope.apps.splice(index, 1);
+    chrome.storage.local.set({'cached': $scope.apps, 'ignored': $scope.ignored});
   }
 
   chrome.storage.local.get(['cached', 'ignored'], function(items) {
     $scope.apps = items.cached || [];
     $scope.ignored = items.ignored || [];
+    for (var i = 0; i < $scope.ignored.length; i++) {
+      $scope.apps.splice($scope.apps.indexOf($scope.ignored[i]), 1);
+    }
   });
 
   $http
     .get('http://localhost/localhost-webapps/server/retrieve.php')
     .success(function(data, status, headers, config) {
       $scope.apps = data;
+      for (var i = 0; i < $scope.ignored.length; i++) {
+        $scope.apps.splice($scope.apps.indexOf($scope.ignored[i]), 1);
+      }
       chrome.storage.local.set({'cached': data});
     });
 });
