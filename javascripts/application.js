52a17ecc844d394650df838bff37be2a7475c4d3
Arnelle Balane <arnellebalane@gmail.com>
2014-01-05 09:14:02 +0800

    can choose which apps to ignore

--- javascripts/application.js
+++ javascripts/application.js
@@ -2,11 +2,18 @@ var app = angular.module('localhost', []);
 
 app.controller('AppsController', function($scope, $http) {
   $scope.apps = [];
+  $scope.ignored = [];
+  $scope.ignore = function(event, index) {
+    event.preventDefault();
+    $scope.ignored.push($scope.apps[index]);
+    chrome.storage.local.set({'ignored': $scope.ignored});
+  }
+
   chrome.storage.local.get(['cached', 'ignored'], function(items) {
-    if (items.cached) {
-      $scope.apps = items.cached;
-    }
+    $scope.apps = items.cached || [];
+    $scope.ignored = items.ignored || [];
   });
+
   $http
     .get('http://localhost/localhost-webapps/server/retrieve.php')
     .success(function(data, status, headers, config) {
